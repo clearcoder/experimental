f94d1cc4d710801cea554e9183c1872e6c4bfab4
Mustafa Munjid <mustafa.munjid@servicerocket.com>
2014-01-05 21:15:13 +0800

    renamed aws credentials file

--- dns/recipes/base.rb
+++ dns/recipes/base.rb
@@ -4,7 +4,9 @@ require 'cgi'
 require 'logger'
 require '../libraries/utils'
 
-AWS.config(YAML.load(File.read("/opt/.mycreds")))
+AWS_CREDENTIALS_FILE = "/opt/.creds"
+
+AWS.config(YAML.load(File.read(AWS_CREDENTIALS_FILE)))
 AWS.config(:logger => Logger.new($stdout))
 AWS.config(:log_level => :debug)
 AWS.config(:log_formatter => AWS::Core::LogFormatter.colored)
