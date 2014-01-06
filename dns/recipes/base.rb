4d4f4784124093ef3c1a25575c2e99bda1e92757
Mustafa Munjid <mustafa.munjid@servicerocket.com>
2014-01-06 21:40:00 +0800

    Fixed AWS.config call.

--- dns/recipes/base.rb
+++ dns/recipes/base.rb
@@ -7,7 +7,7 @@ require '../libraries/utils'
 AWS_CREDENTIALS_FILE = "/opt/.creds"
 
 #AWS.config(YAML.load(File.read(AWS_CREDENTIALS_FILE)))
-AWS.config(node[:dns][:access_key_id],node[:dns][:secret_access_key])
+AWS.config(:access_key_id => node[:dns][:access_key_id], :secret_access_key => node[:dns][:secret_access_key])
 AWS.config(:logger => Logger.new($stdout))
 AWS.config(:log_level => :debug)
 AWS.config(:log_formatter => AWS::Core::LogFormatter.colored)
