7a5cdf4e1b2683f3e30ce871d21e5b4f22ca0f40
Mustafa Munjid <mustafa.munjid@servicerocket.com>
2014-01-06 20:06:12 +0800

    Reformatted attributes file.

--- dns/attributes/dependencies.rb
+++ dns/attributes/dependencies.rb
@@ -1,8 +1,2 @@
-{
-    "dependencies": {
-        "gems": {
-            "eat": "0.1.8",
-            "aws-sdk": "1.31.3"
-        }
-    }
-}
+default[:dependencies][:gems][:eat] = "0.1.8"
+default[:dependencies][:gems][:aws-sdk] = "1.31.3"
